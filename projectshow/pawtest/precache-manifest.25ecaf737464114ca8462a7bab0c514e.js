0aee7ca6d86d65c7a6c86d73cdcd6c5d8fb3997c
zhaoming <zhaoming6163433@163.com>
2018-08-16 13:59:57 +0800

    zeng

---
+++ projectshow/pawtest/precache-manifest.25ecaf737464114ca8462a7bab0c514e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5c9094383aabd244dfd2",
+    "url": "/js/chunk-vendors.8e99d07d.js"
+  },
+  {
+    "revision": "d2c33729b4201e9ab03c",
+    "url": "/js/app.47674136.js"
+  },
+  {
+    "revision": "8092fcb64e07d5daf867",
+    "url": "/js/about.54a0da91.js"
+  },
+  {
+    "revision": "f9b6d78fa6fc804862d1a893d3075adf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d2c33729b4201e9ab03c",
+    "url": "/css/app.a48bfb5d.css"
+  }
+];
